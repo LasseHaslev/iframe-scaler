db5f7fed71c51fd8f118b5fdac15acdec08e2819
Lasse <lasse@haslev.no>
2017-07-31 17:05:46 +0200

    Added full class to es6, but not tested or run tests, so not shure if this works.

--- src/IframeScaler.js
+++ src/IframeScaler.js
@@ -16,4 +16,62 @@ export default class IframeScaler {
         return object;
     }
 
+
+    // Do the scaling of the iframe
+    scaleIframe( iframe ) {
+
+        var element = iframe ? iframe : this.element;
+
+        var percentage = this.calculatePercentage( element );
+        // http://stackoverflow.com/questions/166160/how-can-i-scale-the-content-of-an-iframe
+        element.style.transform = 'scale(' + percentage + ')';
+        element.style.transformOrigin = '0 0';
+
+        // console.log(element.clientHeight * percentage);
+        this.resizeHeight( element, percentage );
+    }
+
+    resizeHeight( element, percentage ) {
+        if ( this.options.upscale || percentage < 1) {
+            var newHeight = -( element.getAttribute('height') - ( element.getAttribute('height') * percentage ) );
+            console.log(newHeight);
+            element.style.marginBottom = newHeight + 'px';
+        }
+        else {
+            element.style.marginBottom = 'auto';
+        }
+    }
+
+    // get the acctual width
+    getAcctualInnerSize( element ) {
+        var computedStyle = getComputedStyle( element );
+        var elementHeight = element.clientHeight;  // height with padding
+        var elementWidth = element.clientWidth;   // width with padding
+
+        elementHeight -= parseFloat(computedStyle.paddingTop) + parseFloat(computedStyle.paddingBottom);
+        elementWidth -= parseFloat(computedStyle.paddingLeft) + parseFloat(computedStyle.paddingRight);
+
+        return {
+            width: elementWidth,
+            height: elementHeight,
+        };
+    }
+
+    // Get the scale percentage 0-1
+    calculatePercentage( element ) {
+
+        var element = element ? element : this.element;
+
+        // Identify the parent
+        var myParent = element.parentElement;
+        // var myParent = this.data.parent ? this.data.parent : element.parentElement;
+
+        // Get the innerWidth
+        var parentInnerSize = this.getAcctualInnerSize(myParent);
+        // Get the percentage width
+        var percentage =  parentInnerSize.width / element.getAttribute('width');
+
+        // Prevent scaling upwards and return value
+        return this.options.upscale || percentage < 1 ? percentage : 1;
+    }
 }
--- test/IframeScaler.js
+++ test/IframeScaler.js
@@ -1,5 +1,7 @@
 import test from 'ava';
 import IframeScaler from '../dist/index';
+import BrowserEnv from 'browser-env';
+BrowserEnv();
 
 test( 'it has default options', t => {
     let scaler = new IframeScaler();
@@ -20,3 +22,37 @@ test( 'it merges options', t => {
         upscale: true,
     } )
 } );
+
+// test( 'can get the actual inner size', t => {
+    // let scaler = new IframeScaler( {
+        // upscale: true,
+    // } );
+
+    // let element = document.createElement( 'div' );
+    // element.style.width = 100 + 'px';
+    // element.style.paddingLeft = 100 + 'px';
+    // element.style.marginBottom = 100 + 'px';
+
+    // document.body.appendChild( element );
+    // console.log(document.body.innerHTML);
+
+    // let test = scaler.getAcctualInnerSize( element );
+    // console.log(test);
+// } );
+
+// test.only( 'it ads margin to compansate with height', t => {
+
+    // let scaler = new IframeScaler( {
+        // upscale: true,
+    // } );
+
+    // let element = document.createElement( 'div' );
+    // element.style.marginBottom = 100 + 'px';
+
+    // console.log(element.getAttribute( 'height' ));
+
+    // scaler.resizeHeight( element, .5 );
+
+    // t.is( element.style.marginBottom, '50px' );
+
+// } );
